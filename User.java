7768c5ea25c6b3101484cca438dff40684d6a614
Vinayak2835 <vinayaknaik4196@gmail.com>
2021-04-26 19:07:01 +0530

    [User.java] Add:User.java program

---
+++ User.java
@@ -0,0 +1,10 @@
+package com.Day13;
+
+public interface User {
+	 /**
+     * contains abstract method
+     * @param pattern
+     * @param input
+     */
+	void userValidate(String pattern, String input);
+}
